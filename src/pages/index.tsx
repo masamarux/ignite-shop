87c718889f274ae35df87760c9958a454e2546d3
Marcelo Alves <alves-marcelo97@hotmail.com>
2022-10-28 20:41:41 -0300

    add: stripe, ssg on home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,79 @@
-import { styled } from '../styles'
+import Image from 'next/image';
+import { GetStaticProps } from 'next';
+import { useKeenSlider } from 'keen-slider/react'
+import { getPlaiceholder } from "plaiceholder";
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 'none',
-  padding: '4px 8px',
+import { stripe } from '../lib/stripe';
+import { HomeContainer, Product } from '../styles/pages/home';
 
-  span: {
-    fontWeight: 'bold',
-  },
+import 'keen-slider/keen-slider.min.css'
+import Stripe from 'stripe';
 
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  }
-})
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: string
+    blurDataUrl: string
+    price: number
+  }[]
+}
 
-export default function Home() {
+
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2.7,
+      spacing: 48
+    }
+    
+  })
   return (
-    <Button>
-      <span>teste</span>
-      Enviar
-    </Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {
+        products.map(product => (
+          <Product key={product.id} className='keen-slider__slide'>
+            <Image placeholder='blur' blurDataURL={product.blurDataUrl} src={product.imageUrl} width={520} height={480} alt="" />
+
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        ))
+      }
+
+      
+    </HomeContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  });
+
+  const products = response.data.map(async(product) => {
+    const price = product.default_price as Stripe.Price;
+    const { base64, img } = await getPlaiceholder(product.images[0]);
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: img,
+      blurDataUrl: base64,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount / 100)
+    }
+  });
+
+  return {
+    props: {
+      products: await Promise.all(products),
+    },
+    revalidate: 60 * 60 * 2 // 2 hours
+  }
 }
